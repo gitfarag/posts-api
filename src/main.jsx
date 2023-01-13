939617cf750f9b105b5f140067219e3e1b19e111
gitfarag <react.codework@gmail.com>
2023-01-13 21:21:42 +0200

    first from new branch

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <PostsContextProvider>
     <App />
-    </PostsContextProvider> 
+  </PostsContextProvider>
   </React.StrictMode>,
 )
